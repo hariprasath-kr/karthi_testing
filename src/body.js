cf8ca5e3b965b9c4aa84a70b4a56c02ff7c96c6c
HARI PRASATH K R <122036470+hariprasath-kr@users.noreply.github.com>
2023-04-13 19:40:45 +0530

    finall

--- src/body.js
+++ src/body.js
@@ -5,19 +5,20 @@ const Body = () => {
             <p className="content">
                <h1> Where the Power of Block Chain Meets Utility , Creating a world of endless possibilities </h1>
             </p>
-            <div class="card text-center">
-  <div class="card-header">
-    Featured
-  </div>
-  <div className="card-body">
-    <h5 className="card-title">Special title treatment</h5>
-    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    
-  </div>
-  <div className="card-footer text-body-secondary">
-    2 days ago
-  </div>
-</div>
+           
+                    <div class="card text-center bg-black text-white bdbx  ">
+                        <div class="card-header ">
+                          Featured
+                        </div>
+                        <div className="card-body">
+                          <h5 className="card-title">Special title treatment</h5>
+                          <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        </div>
+                        <div className="card-footer ">
+                          2 days ago
+                        </div>
+                     </div>
+           
         </div>
      );
 }
--- src/header.js
+++ src/header.js
@@ -2,20 +2,24 @@ import logo from "./allar.png"
 const Header = () => {
     let url="";
     return ( 
-        <div className="heaader">
-           <div className="logo">
-                <img src={logo} alt={"logo"}/>
-           </div>
-            <div className="buttons">
-                <ul>
-                    <li><a href={url}>{"How it Works"}</a></li>
-                    <li><a href={url}>{"Contact Us"}</a></li>
-                    <li><a href={url}>{"About Us"}</a></li>
-                    <li><a href={url}>{"Team"}</a></li>
-                    <li><a href={url}>{"White Papper"}</a></li>
-                    
-                </ul>
-            </div>
+        <div>
+            <ul className="nav justify-content-end ">
+                <li className="nav-item">
+                    <a className="nav-link active text-black" aria-current="page" href="#">Active</a>
+                </li>
+                <li className="nav-item ">
+                    <a className="nav-link text-black" href="#">Link</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link text-black" href="#">Link</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link text-black">Disabled</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link text-black">Disabled</a>
+                </li>
+            </ul>
         </div>
     );
 }
